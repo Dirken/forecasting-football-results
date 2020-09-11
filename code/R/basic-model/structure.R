341f0fbe6389d4b1a1ee65a17f702fe435a0e530
RMeyerhofer <ricard.meyerhofer@est.fib.upc.edu>
2020-09-11 05:24:04 +0200

    re-organizing

--- code/R/basic-model/structure.R
+++ code/R/basic-model/structure.R
@@ -1,19 +1,15 @@
+#########################################################
+#General
+#########################################################
 source("libraries.R")
 source("loadingDB.R")
 source("missingValues.R")
-
-
 
 #########################################################
 #Base Model
 #########################################################
 #factor analysis: does possesion imply win? Do red cards imply more wins?
 source("factorStudies.R")
-
-
-
-
-
 
 #########################################################
 # Maybe useful? Don't think so
